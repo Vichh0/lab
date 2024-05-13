670c3b11d85c2a0c6d5d47b3c137828e4d8c9a59
Vichh0 <vicentecastillomedina44@gmail.com>
2024-05-13 18:40:28 -0400

    dfa

--- lib/pages/newfile.dart
+++ lib/pages/newfile.dart
@@ -117,6 +117,26 @@ class Home extends StatelessWidget {
         ),
       ],
       body: Center(
+        ListView(
+          padding: const EdgeInsets.all(8),
+          children: <Widget>[
+            Container(
+              height: 50,
+              color: Colors.amber[600],
+              child: const Center(child: Text('Entry A')),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[500],
+              child: const Center(child: Text('Entry B')),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[100],
+              child: const Center(child: Text('Entry C')),
+            ),
+          ],
+        ),
       ),
     );
   }
