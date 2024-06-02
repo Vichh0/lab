372125bd78b86c2ccdc60caad65cc950c7368cde
Vichh0 <vicentecastillomedina44@gmail.com>
2024-06-02 02:20:04 -0400

    ?

--- lib/Data/Videojuegos.dart
+++ lib/Data/Videojuegos.dart
@@ -1,8 +1,11 @@
 class Videojuegos{
+
   String names = "";
   String genero = "";
   String info = "";
+
   Videojuegos(this.names,this.genero,this.info);
+  
   void annadir(){}
   void borrar(){}
 }
